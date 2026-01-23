f176e97d7faa63af27e8a9d3e766310822f46698
Christian Flothmann <christian.flothmann@open.de>
2026-01-23 18:50:14 +0100

    revert clean ups that make tests incompatible with Symfony 7.4

--- Tests/Functional/SecurityTest.php
+++ Tests/Functional/SecurityTest.php
@@ -248,6 +248,11 @@ final class UserWithoutEquatable implements UserInterface, PasswordAuthenticated
     {
         return $this->enabled;
     }
+
+    #[\Deprecated]
+    public function eraseCredentials(): void
+    {
+    }
 }
 
 class ForceLoginController
